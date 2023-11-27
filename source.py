4c0e229277083d41d87eca45c2ecc6176356e3e6
Jeremy L. Shepherd <jeremylshepherd@gmail.com>
2023-11-26 23:53:37 -0500

    ML - create training/testing sets

--- source.py
+++ source.py
@@ -33,22 +33,31 @@
 # *How will you use the identified data to answer your project question?*
 # 📝 <!-- Start Discussing the project here; you can add as many code cells as you need -->
 
-# In[1]:
+# In[29]:
 
 
 # Start your code here
 # Not sure what the expectation is for this block
 import numpy as np
 import pandas as pd
+import datetime
 from scipy.stats import trim_mean
 
 # Configure pandas to display 500 rows; otherwise it will truncate the output
 pd.set_option('display.max_rows', 500)
 
+import os
+
+# Scikit Learn imports
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import make_pipeline
+
 # To plot pretty figures
 get_ipython().run_line_magic('matplotlib', 'inline')
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import seaborn as sns
 mpl.rc('axes', labelsize=14)
 mpl.rc('xtick', labelsize=12)
 mpl.rc('ytick', labelsize=12)
@@ -70,7 +79,7 @@ seattle02_crime_df.head()
 # Data visualizations to evaluate the data in order to form conclusions about whether marijuana legalization had a tangential impact on other aspects of crime.
 # I am paying particular attention to the 2-3 year period before and after legalization.
 
-# In[2]:
+# In[30]:
 
 
 # Get the shapes of the data
@@ -80,7 +89,7 @@ display(seattle02_crime_df.shape)
 display(kc_crime_df.shape)
 
 
-# In[3]:
+# In[31]:
 
 
 # Look at the columns and compare the presentation of the data
@@ -93,21 +102,21 @@ display(kc_crime_df.info())
 # ### Intitial thoughts
 # The data from Seattle 01 and Kansas City do not appear to provide much in the way of usable data. Denver and Seattle 02 show some promise. The datasets are structured differently but I may be able to extract the necessary data from them to answer the question I am researching. I will need to try to find another comparison city for evaluation against the cities with legalized marijuana.
 
-# In[4]:
+# In[32]:
 
 
 # Check for duplicated records for Denver
 denver_crime_df.duplicated().sum()
 
 
-# In[5]:
+# In[33]:
 
 
 # Check for duplicated records for Seattle
 seattle02_crime_df.duplicated().sum()
 
 
-# In[6]:
+# In[34]:
 
 
 # Drop duplicates from Seattle and rename
@@ -116,7 +125,7 @@ seattle_crime_df = seattle02_crime_df.drop_duplicates()
 display(seattle_crime_df.shape)
 
 
-# In[7]:
+# In[35]:
 
 
 # Check for missing values for Denver
@@ -130,13 +139,13 @@ display(seattle_crime_df.isna().sum())
 # ### Statistical Summary of the Data
 # Below is the statistical summary of the data for Denver and Seattle.
 
-# In[8]:
+# In[36]:
 
 
 denver_crime_df.describe()
 
 
-# In[9]:
+# In[37]:
 
 
 seattle_crime_df.describe()
@@ -145,31 +154,31 @@ seattle_crime_df.describe()
 # ### Data visualizations
 # Display a representation of the data to evaluate best way to evaluate it.
 
-# In[10]:
+# In[38]:
 
 
 denver_crime_df.hist(figsize=(20,15), bins=50)
 
 
-# In[11]:
+# In[39]:
 
 
 seattle_crime_df.hist(figsize=(20,15), bins=50)
 
 
-# In[12]:
+# In[40]:
 
 
 seattle_crime_df['CRIME_TYPE'].hist(figsize=(20,15), bins=50)
 
 
-# In[13]:
+# In[41]:
 
 
 denver_crime_df['offense_category_id'].hist(figsize=(20,15), bins=50)
 
 
-# In[17]:
+# In[42]:
 
 
 simplified_denver_crime_df = denver_crime_df.drop(['incident_id','offense_id','offense_code','offense_code_extension','first_occurrence_date',
@@ -179,27 +188,69 @@ simplified_denver_crime_df = denver_crime_df.drop(['incident_id','offense_id','o
 simplified_denver_crime_df['offense_category_id'].value_counts().plot(kind='barh')
 
 
-# In[15]:
+# In[43]:
 
 
 seattle_scatter_mat = pd.plotting.scatter_matrix(seattle_crime_df, figsize=(20,15))
 seattle_scatter_mat
 
 
-# In[18]:
+# In[44]:
 
 
 seattle_crime_df['CRIME_TYPE'].value_counts().plot(kind='barh')
 
 
-# # First Impressions
+# ### First Impressions/Conclusions
 # Scatter Matrixes were not particularly helpful. I see that the primary data I want is the total crime type by year. I will need to eliminate unneeded columns and so some data transformation to make the sets more useful. I have trimmed down known extraneous columns from denver just reduce the load on the data viz libraries. I will refine further during the next phase.
+
+# ## Machine Learning and Regression
+# ### Data transformation
+# I will add a column to each dataset exclusively for the year of the offense. This should allow me to observe the rate of change, if any, for each type year over year. The datasets have different categories of crimes that do not necessarily correspond to each other. However, I do not see this necessarily as a hindrance as the real intent is to see if there are any changes in the respective categories.
+
+# In[48]:
+
+
+# Transform Denver data
+simplified_denver_crime_df['reported_datetime'] = pd.to_datetime(simplified_denver_crime_df['reported_date'])
+simplified_denver_crime_df.head()
+simplified_denver_crime_df['year'] = simplified_denver_crime_df['reported_datetime'].dt.year
+simplified_denver_crime_df.head()
+
+
+# In[51]:
+
+
+seattle_crime_df['REPORT_DATETIME'] = pd.to_datetime(seattle_crime_df['REPORT_DATE'])
+seattle_crime_df.head()
+seattle_crime_df['YEAR'] = seattle_crime_df['REPORT_DATETIME'].dt.year
+seattle_crime_df.head()
+
+
+# ### Remove all superflous columns
+# Remove unnecessary columns from the datasets to maximize focus on important data.
+
+# In[55]:
+
+
+denver_crime_df_v2 = simplified_denver_crime_df.drop(['offense_type_id', 'reported_date',
+       'reported_datetime'], axis=1)
+denver_crime_df_v2.head()
+
+
+# In[57]:
+
+
+seattle_crime_df_v2 = seattle_crime_df.drop(['Police Beat', 'CRIME_DESCRIPTION', 'STAT_VALUE', 'REPORT_DATE',
+                                             'Sector', 'Precinct', 'Row_Value_ID', 'REPORT_DATETIME'] , axis=1)
+seattle_crime_df_v2.head()
+
 
 # ## Resources and References
 # *What resources and references have you used for this project?*
 # I used Kaggle to source the datasets
 
-# In[19]:
+# In[46]:
 
 
 # ⚠️ Make sure you run this cell at the end of your notebook before every submission!
